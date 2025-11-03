d77652b9c1214c21a3ea5926d06be7786e5d0baa
aiger555 <aigerim.nuralieva@iaau.edu.kg>
2025-11-03 22:49:23 +0600

    swagger

--- insurance-service/src/main/java/com/ain/insuranceservice/controllers/DriverController.java
+++ insurance-service/src/main/java/com/ain/insuranceservice/controllers/DriverController.java
@@ -6,9 +6,12 @@ import com.ain.insuranceservice.dto.DriverRequestDTO;
 import com.ain.insuranceservice.dto.DriverResponseDTO;
 import com.ain.insuranceservice.services.ClientService;
 import com.ain.insuranceservice.services.DriverService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,31 +20,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@Data
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/drivers")
+@Tag(name = "Driver", description = "API for managing Drivers") // swagger
 public class DriverController {
     private final DriverService driverService;
 
     @GetMapping
+    @Operation(summary = "Get Drivers") // swagger
     public ResponseEntity<List<DriverResponseDTO>> getDrivers() {
         List<DriverResponseDTO> drivers = driverService.getDrivers();
         return new ResponseEntity<>(drivers, HttpStatus.OK);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Driver") // swagger
     public ResponseEntity<DriverResponseDTO> createDriver(@Valid @RequestBody DriverRequestDTO driverRequestDTO) {
         DriverResponseDTO driverResponseDTO = driverService.createDriver(driverRequestDTO);
         return new ResponseEntity<>(driverResponseDTO, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a Driver") // swagger
     public ResponseEntity<DriverResponseDTO> updateDriver(@PathVariable UUID id, @Validated({Default.class}) @RequestBody DriverRequestDTO driverRequestDTO) {
         DriverResponseDTO driverResponseDTO = driverService.updateDriver(id, driverRequestDTO);
         return new ResponseEntity<>(driverResponseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Driver") // swagger
     public ResponseEntity<Void> deleteDriver(@PathVariable UUID id) {
         driverService.deleteDriver(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- insurance-service/src/main/java/com/ain/insuranceservice/controllers/InsurancePolicyController.java
+++ insurance-service/src/main/java/com/ain/insuranceservice/controllers/InsurancePolicyController.java
@@ -4,9 +4,12 @@ import com.ain.insuranceservice.dto.*;
 import com.ain.insuranceservice.models.InsurancePolicy;
 import com.ain.insuranceservice.services.ClientService;
 import com.ain.insuranceservice.services.InsurancePolicyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,31 +19,36 @@ import java.util.List;
 import java.util.UUID;
 
 
-@Data
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/policies")
+@Tag(name = "Policy", description = "API for managing Policies") // swagger
 public class InsurancePolicyController {
     private final InsurancePolicyService policyService;
 
     @GetMapping
+    @Operation(summary = "Get Policies") // swagger
     public ResponseEntity<List<InsurancePolicyResponseDTO>> getPolicies() {
         List<InsurancePolicyResponseDTO> policies = policyService.getInsurancePolicies();
         return new ResponseEntity<>(policies, HttpStatus.OK);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Policy") // swagger
     public ResponseEntity<InsurancePolicyResponseDTO> createPolicy(@Valid @RequestBody InsurancePolicyRequestDTO policyRequestDTO) {
         InsurancePolicyResponseDTO policyResponseDTO = policyService.createInsurancePolicy(policyRequestDTO);
         return new ResponseEntity<>(policyResponseDTO, HttpStatus.CREATED);
     }
 
     @PutMapping("/{policyNumber}")
+    @Operation(summary = "Update a Policy") // swagger
     public ResponseEntity<InsurancePolicyResponseDTO> updatePolicy(@PathVariable String policyNumber, @Validated({Default.class}) @RequestBody InsurancePolicyRequestDTO insurancePolicyRequestDTO) {
         InsurancePolicyResponseDTO insurancePolicyResponseDTO = policyService.updatePolicy(policyNumber, insurancePolicyRequestDTO);
         return new ResponseEntity<>(insurancePolicyResponseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Policy") // swagger
     public ResponseEntity<Void> deletePolicy(@PathVariable String id) {
         policyService.deletePolicy(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
