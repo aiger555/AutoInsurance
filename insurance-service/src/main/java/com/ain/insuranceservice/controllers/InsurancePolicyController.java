c3d7d92a501dd6350fbe1f1011c425a4484a1b74
aiger555 <aigerim.nuralieva@iaau.edu.kg>
2025-11-23 16:34:52 +0600

    fixed err

--- insurance-service/src/main/java/com/ain/insuranceservice/controllers/InsurancePolicyController.java
+++ insurance-service/src/main/java/com/ain/insuranceservice/controllers/InsurancePolicyController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/policies")
